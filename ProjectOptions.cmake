f02cdc6f564b81c583a9afc1df3b6856afb8ef9d
Template Janitor <template.janitor@example.com>
2025-10-23 06:42:24 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(MyGhostEscape_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
 
     message(STATUS "Sanity checking UndefinedBehaviorSanitizer, it should be supported on this platform")
@@ -54,183 +54,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(MyGhostEscape_setup_options)
+  option(MyGhostEscape_ENABLE_HARDENING "Enable hardening" ON)
+  option(MyGhostEscape_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    MyGhostEscape_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    MyGhostEscape_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  MyGhostEscape_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR MyGhostEscape_PACKAGING_MAINTAINER_MODE)
+    option(MyGhostEscape_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(MyGhostEscape_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(MyGhostEscape_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyGhostEscape_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(MyGhostEscape_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(MyGhostEscape_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyGhostEscape_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(MyGhostEscape_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(MyGhostEscape_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(MyGhostEscape_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(MyGhostEscape_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(MyGhostEscape_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyGhostEscape_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyGhostEscape_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(MyGhostEscape_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(MyGhostEscape_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyGhostEscape_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      MyGhostEscape_ENABLE_IPO
+      MyGhostEscape_WARNINGS_AS_ERRORS
+      MyGhostEscape_ENABLE_USER_LINKER
+      MyGhostEscape_ENABLE_SANITIZER_ADDRESS
+      MyGhostEscape_ENABLE_SANITIZER_LEAK
+      MyGhostEscape_ENABLE_SANITIZER_UNDEFINED
+      MyGhostEscape_ENABLE_SANITIZER_THREAD
+      MyGhostEscape_ENABLE_SANITIZER_MEMORY
+      MyGhostEscape_ENABLE_UNITY_BUILD
+      MyGhostEscape_ENABLE_CLANG_TIDY
+      MyGhostEscape_ENABLE_CPPCHECK
+      MyGhostEscape_ENABLE_COVERAGE
+      MyGhostEscape_ENABLE_PCH
+      MyGhostEscape_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  MyGhostEscape_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (MyGhostEscape_ENABLE_SANITIZER_ADDRESS OR MyGhostEscape_ENABLE_SANITIZER_THREAD OR MyGhostEscape_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(MyGhostEscape_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(MyGhostEscape_global_options)
+  if(MyGhostEscape_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    MyGhostEscape_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  MyGhostEscape_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyGhostEscape_ENABLE_HARDENING AND MyGhostEscape_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyGhostEscape_ENABLE_SANITIZER_UNDEFINED
+       OR MyGhostEscape_ENABLE_SANITIZER_ADDRESS
+       OR MyGhostEscape_ENABLE_SANITIZER_THREAD
+       OR MyGhostEscape_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${MyGhostEscape_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${MyGhostEscape_ENABLE_SANITIZER_UNDEFINED}")
+    MyGhostEscape_enable_hardening(MyGhostEscape_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(MyGhostEscape_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(MyGhostEscape_warnings INTERFACE)
+  add_library(MyGhostEscape_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  MyGhostEscape_set_project_warnings(
+    MyGhostEscape_warnings
+    ${MyGhostEscape_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(MyGhostEscape_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    MyGhostEscape_configure_linker(MyGhostEscape_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  MyGhostEscape_enable_sanitizers(
+    MyGhostEscape_options
+    ${MyGhostEscape_ENABLE_SANITIZER_ADDRESS}
+    ${MyGhostEscape_ENABLE_SANITIZER_LEAK}
+    ${MyGhostEscape_ENABLE_SANITIZER_UNDEFINED}
+    ${MyGhostEscape_ENABLE_SANITIZER_THREAD}
+    ${MyGhostEscape_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(MyGhostEscape_options PROPERTIES UNITY_BUILD ${MyGhostEscape_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(MyGhostEscape_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      MyGhostEscape_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(MyGhostEscape_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    MyGhostEscape_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(MyGhostEscape_ENABLE_CLANG_TIDY)
+    MyGhostEscape_enable_clang_tidy(MyGhostEscape_options ${MyGhostEscape_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(MyGhostEscape_ENABLE_CPPCHECK)
+    MyGhostEscape_enable_cppcheck(${MyGhostEscape_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(MyGhostEscape_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    MyGhostEscape_enable_coverage(MyGhostEscape_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(MyGhostEscape_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(MyGhostEscape_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyGhostEscape_ENABLE_HARDENING AND NOT MyGhostEscape_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyGhostEscape_ENABLE_SANITIZER_UNDEFINED
+       OR MyGhostEscape_ENABLE_SANITIZER_ADDRESS
+       OR MyGhostEscape_ENABLE_SANITIZER_THREAD
+       OR MyGhostEscape_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    MyGhostEscape_enable_hardening(MyGhostEscape_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
 
 macro(
-  myproject_enable_hardening
+  MyGhostEscape_enable_hardening
   target
   global
   ubsan_minimal_runtime)
