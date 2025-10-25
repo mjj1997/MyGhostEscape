556b440a0e3c2ac6d37315e324189333f9d262b7
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2025-10-25 10:23:10 +0800

    feat: Add init() and clean() to initialize and deinitialize SDL libraries

---
+++ src/game.cpp
@@ -0,0 +1,66 @@
+#include "game.h"
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+void Game::init(std::string_view title, int width, int height)
+{
+    m_screenSize = glm::vec2{ width, height };
+
+    // SDL3初始化
+    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL初始化失败: %s\n", SDL_GetError());
+        m_isRunning = false;
+    }
+
+    // 不需要进行SDL_image初始化
+
+    // SDL3_Mixer初始化
+    if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) != (MIX_INIT_MP3 | MIX_INIT_OGG)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Mixer初始化失败: %s\n", SDL_GetError());
+        m_isRunning = false;
+    }
+    if (!Mix_OpenAudio(0, NULL)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Mixer打开音频失败: %s\n", SDL_GetError());
+        m_isRunning = false;
+    }
+    Mix_AllocateChannels(16); // 分配16个音频通道
+
+    // SDL3_TTF初始化
+    if (!TTF_Init()) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_TTF初始化失败: %s\n", SDL_GetError());
+        m_isRunning = false;
+    }
+
+    // 创建窗口与渲染器
+    SDL_CreateWindowAndRenderer(title.data(),
+                                width,
+                                height,
+                                SDL_WINDOW_RESIZABLE,
+                                &m_window,
+                                &m_renderer);
+    if (!m_window || !m_renderer) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "创建窗口或渲染器失败: %s\n", SDL_GetError());
+        m_isRunning = false;
+    }
+    // 设置窗口逻辑分辨率
+    SDL_SetRenderLogicalPresentation(m_renderer, width, height, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+}
+
+void Game::clean()
+{
+    // 关闭SDL3_TTF
+    TTF_Quit();
+    // 关闭SDL3_Mixer
+    Mix_CloseAudio();
+    Mix_Quit();
+    // 销毁窗口与渲染器
+    if (m_renderer)
+        SDL_DestroyRenderer(m_renderer);
+    if (m_window)
+        SDL_DestroyWindow(m_window);
+    // 退出SDL3
+    SDL_Quit();
+}
--- src/game.h
+++ src/game.h
@@ -1,16 +1,32 @@
 #pragma once
 
+#include <SDL3/SDL.h>
+#include <glm/glm.hpp>
+
+#include <string_view>
+
 class Game
 {
 public:
+    ~Game() = default;
+
     static Game& instance()
     {
         static Game instance;
         return instance;
     }
 
+    void init(std::string_view title, int width, int height);
+    void clean();
+
 private:
     Game() = default;
     Game(const Game&) = delete;
     Game& operator=(const Game&) = delete;
+
+    glm::vec2 m_screenSize{ glm::vec2{ 0.0f } }; // 屏幕尺寸
+    bool m_isRunning{ true };
+
+    SDL_Window* m_window{ nullptr };
+    SDL_Renderer* m_renderer{ nullptr };
 };
