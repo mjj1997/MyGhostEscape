fb848821836ba656213aeaf67603b4c5681cb9a8
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2025-11-04 11:12:43 +0800

    refactor: Update SceneMain class to use object tree system

--- src/scene_main.cpp
+++ src/scene_main.cpp
@@ -5,30 +5,32 @@ void SceneMain::init()
 {
     m_worldSize = m_game.screenSize() * 3.0f;                           // 世界大小为屏幕大小的3倍
     m_cameraPosition = m_worldSize / 2.0f - m_game.screenSize() / 2.0f; // 初始相机位置
+
     m_player = new Player;
     m_player->init();
     m_player->setWorldPosition(m_worldSize / 2.0f);
+    addChild(m_player);
 }
 
-void SceneMain::handleEvents(SDL_Event& event) {}
+void SceneMain::handleEvents(SDL_Event& event)
+{
+    Scene::handleEvents(event);
+}
 
 void SceneMain::update(float deltaTime)
 {
-    m_player->update(deltaTime);
+    Scene::update(deltaTime);
 }
 
 void SceneMain::render()
 {
+    Scene::render();
     renderBackground();
-    m_player->render();
 }
 
 void SceneMain::clean()
 {
-    if (m_player != nullptr) {
-        m_player->clean();
-        delete m_player;
-    }
+    Scene::clean();
 }
 
 void SceneMain::renderBackground()
